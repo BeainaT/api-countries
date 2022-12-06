7e95f3faf5c3cd458bf4489cd002a7b82f7571eb
BeainaT <tania.df@hotmail.it>
2022-12-06 16:42:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import MainHeader from './components/MainHeader.vue';
+</script>
+
+<template>
+  <header>
+    <MainHeader/>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style lang="scss" scoped>
+$red: #ff0000;
+header {
+  color: $red;
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.scss'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/css/bootstrap-reboot.min.css'
+
+createApp(App).mount('#app')
