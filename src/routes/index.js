12e95d968e3d07ffcdc7ad046fa373d011a5b57b
BeainaT <tania.df@hotmail.it>
2026-03-23 14:56:24 +0100

    added scroll on routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,9 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
-  })
+    scrollBehavior() {
+        return { top: 0, behavior: 'instant' }
+    }
+})
 
 export default router;
